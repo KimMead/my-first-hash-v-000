d02cd059a51b9c188b7e55cd36a142e07130ef4e
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-03-31 20:57:17 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,7 +3,7 @@ your_hash = {"cat" => "Karen", "dog" => "Mama"}
 end
 
 def shipping_manifest
-the_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
+the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
@@ -13,7 +13,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 end
-shipping_manifest[oil paintings]
+
 
 def adding
   shipping_manifest = {
